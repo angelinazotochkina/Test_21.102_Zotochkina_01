4998d5f83acd3eba48aa96c22fe8561d5225d4ac
Angelina <angelina.zotochkina@yandex.ru>
2024-07-01 22:47:59 +0700

    Добавьте файлы проекта.

---
+++ 21.102_Zotochkina_01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21._102_Zotochkina_01
+{
+ public class Program
+    {
+       public static int NMin(double[] A, int N)
+        {
+            double min = A[0];
+            int minIndex = 0;
+
+            for (int i = 1; i < N; i++)
+            {
+                if (A[i] < min)
+                {
+                    min = A[i];
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+     public   static int NMax(double[] A, int N)
+        {
+            double max = A[0];
+            int maxIndex = 0;
+
+            for (int i = 1; i < N; i++)
+            {
+                if (A[i] > max)
+                {
+                    max = A[i];
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+
+     public   static void Main()
+        {
+            double[] array = { 3.14, 2.71, 1.618, 0.0, 42.0 }; // пример массива
+
+            int minIndex = NMin(array, array.Length);
+            int maxIndex = NMax(array, array.Length);
+
+            Console.WriteLine("Индекс минимального элемента: " + minIndex);
+            Console.WriteLine("Индекс максимального элемента: " + maxIndex);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using _21._102_Zotochkina_01;
+
+ namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestNMin()
+        {
+            // Arrange
+            double[] array = { 3.14, 2.71, 1.618, 0.0, 42.0 };
+
+            // Act
+            int minIndex = Program.NMin(array, array.Length);
+
+            // Assert
+            Assert.AreEqual(3, minIndex); // Предполагаем, что минимальный элемент массива находится по индексу 3
+        }
+
+        [TestMethod]
+        public void TestNMax()
+        {
+            // Arrange
+            double[] array = { 3.14, 2.71, 1.618, 0.0, 42.0 };
+
+            // Act
+            int maxIndex = Program.NMax(array, array.Length);
+
+            // Assert
+            Assert.AreEqual(4, maxIndex); // Предполагаем, что максимальный элемент массива находится по индексу 4
+        }
+
+        [TestMethod]
+        public void TestNMinAndMaxSameElement()
+        {
+            // Arrange
+            double[] array = { 3.14, 2.71, 1.618, 42.0, 42.0 };
+
+            // Act
+            int minIndex = Program.NMin(array, array.Length);
+            int maxIndex = Program.NMax(array, array.Length);
+
+            // Assert
+            Assert.AreEqual(2, minIndex); // Предполагаем, что минимальный элемент массива находится по индексу 2
+            Assert.AreEqual(3, maxIndex); // Предполагаем, что максимальный элемент массива находится по индексу 3
+        }
+    }
+}
+
